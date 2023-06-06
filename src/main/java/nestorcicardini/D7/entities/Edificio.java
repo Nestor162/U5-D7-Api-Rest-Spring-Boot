ab5ba6403a7d7fad33a8c95e42eef34cdb5ebd9a
Nestor162 <nestorcicardini@gmail.com>
2023-06-06 14:55:31 +0200

    Creata classe Edificio

---
+++ src/main/java/nestorcicardini/D7/entities/Edificio.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D7.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Edificio {
+	private String indirizzo;
+	private String nome;
+	private String citta;
+
+}
