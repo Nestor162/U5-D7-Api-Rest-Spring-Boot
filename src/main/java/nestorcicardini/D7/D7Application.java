cd850ab2d712a4e124d9c4605c2dd9eaafa87e9b
Nestor162 <nestorcicardini@gmail.com>
2023-06-06 14:34:37 +0200

    Caricati file iniziali progetto

---
+++ src/main/java/nestorcicardini/D7/D7Application.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D7Application.class, args);
+	}
+
+}
---
+++ src/test/java/nestorcicardini/D7/D7ApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
